e60278d884fea13164a9445e08ff089c6e213b89
Yasin Zähringer <yasinzaehringer@yhjz.de>
2017-05-30 13:39:33 +0100

    improved the permission model

--- wphotos/permissions.py
+++ wphotos/permissions.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AnonymousUser
 from rest_framework import permissions
 
 
@@ -6,6 +7,14 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     Object-level permission to only allow owners of an object to edit it.
     Assumes the model instance has an `owner` attribute.
     """
+    def has_permission(self, request, view):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Otherwise the user has to be authenticated.
+        return request.user.is_authenticated()
 
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request,
@@ -21,6 +30,18 @@ class IsUserOrReadOnly(permissions.BasePermission):
     """
     Object-level permission to only allow the user themself to edit the user object.
     """
+    def has_permission(self, request, view):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # do not let the user create new users
+        if request.method == 'POST':
+            return False
+
+        # Otherwise the user has to be authenticated.
+        return request.user.is_authenticated()
 
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request,
@@ -28,5 +49,4 @@ class IsUserOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        # Instance must have an attribute named `owner`.
         return obj == request.user
