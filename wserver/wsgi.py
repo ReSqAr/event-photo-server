5fb821f97e8913b93d03f3f4f61f7fbe042b4b44
Yasin Zähringer <yasinzaehringer@yhjz.de>
2017-06-12 16:40:46 +0100

    wsgi fix

--- wserver/wsgi.py
+++ wserver/wsgi.py
@@ -8,8 +8,15 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+# sudo apt-get install python3-dev libjpeg8-dev
+# pip3 install django djangorestframework typing Pillow
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wserver.settings")
 
