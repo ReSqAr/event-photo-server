9935b783ce1e66e138ba18ee1b537136a0b38de2
Yasin Zähringer <yasinzaehringer@yhjz.de>
2017-06-26 23:25:33 +0100

    Changed URLs

--- wserver/urls.py
+++ wserver/urls.py
@@ -30,18 +30,12 @@ router.register(r'comments', views.CommentViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api-token-auth/', rest_views.obtain_auth_token),
-    url(r'^api-information/', views.server_information),
-    url(r'^create-user/', views.create_user),
-    url(r'^api-create-user/', views.create_user),
 
-    url(r'^api/', include(router.urls)),
+    url(r'^api/create-user/', views.create_user),
     url(r'^api/auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/token-auth/', rest_views.obtain_auth_token),
     url(r'^api/information/', views.server_information),
-    url(r'^api/create-user/', views.create_user),
+    url(r'^api/', include(router.urls)),
 ]
 
 if settings.DEBUG:
